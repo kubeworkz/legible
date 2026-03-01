20ce1f91d14ff617866ec8f8886c8f12e838547c
Legible <dave@gridworkz.com>
2026-03-01 01:49:16 +0000

    add sidenav styles

--- wren-ui/src/components/LogoBar.tsx
+++ wren-ui/src/components/LogoBar.tsx
@@ -1,12 +1,22 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { Path, buildPath } from '@/utils/enum';
+import useProject from '@/hooks/useProject';
 
 export default function LogoBar() {
+  const { currentProjectId } = useProject();
+  const homePath = currentProjectId
+    ? buildPath(Path.Home, currentProjectId)
+    : '/';
+
   return (
-    <Image
-      src="/images/logo-white-with-text.svg"
-      alt="Wren AI"
-      width={125}
-      height={30}
-    />
+    <Link href={homePath} style={{ display: 'inline-flex' }}>
+      <Image
+        src="/images/logo-white-with-text.svg"
+        alt="Wren AI"
+        width={125}
+        height={30}
+      />
+    </Link>
   );
 }
